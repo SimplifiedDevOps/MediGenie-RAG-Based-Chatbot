3873a462fdf9e72e3392900ced4599fdad8cbe0a
Ashwani Vardhineni <avardhineni7@gmail.com>
2024-10-19 05:42:43 +0530

    Created Initial Project Structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MediGenie-RAG-Based-ChatbotMediGenie-RAG-Based-Chatbot',
+    version= '1.0.0',
+    author= 'Ashwani Vardhineni',
+    author_email= 'avardhineni7@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
